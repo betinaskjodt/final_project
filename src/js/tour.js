7a651ea4294b15464cec04750bb36b866629ead8
betinaskjodt <betina_horthe@hotmail.com>
2024-12-04 14:12:42 +0100

    starting tour page with structure, style and some logic. logic not finished

--- src/js/tour.js
+++ src/js/tour.js
@@ -3,296 +3,448 @@ const tours = [
     date: 6,
     month: "janurary",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Zayed Sports City Stadium",
+    city: "Abu Dhabi",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 8,
     month: "janurary",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Zayed Sports City Stadium",
+    city: "Abu Dhabi",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 24,
     month: "janurary",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Kai Tak Stadium",
+    city: "Hong Kong",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 26,
     month: "janurary",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Kai Tak Stadium",
+    city: "Hong Kong",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 20,
     month: "march",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Goyang Stadium",
+    city: "Seoul",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 22,
     month: "march",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Goyang Stadium",
+    city: "Seoul",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 1,
     month: "April",
     year: 2025,
-    location: "",
-    country: "",
+    location: "D Y Patil Stadium",
+    city: "Mumbai",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 2,
     month: "april",
     year: 2025,
-    location: "",
-    country: "",
+    location: "D Y Patil Stadium",
+    city: "Mumbai",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 6,
     month: "april",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Wembley Stadium",
+    city: "London",
+    continent: "Europa",
     availableTickets: false,
   },
   {
     date: 8,
     month: "april",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Wembley Stadium",
+    city: "London",
+    continent: "Europa",
     availableTickets: false,
   },
   {
     date: 10,
     month: "may",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Wembley Stadium",
+    city: "London",
+    continent: "Europa",
     availableTickets: true,
   },
   {
     date: 11,
     month: "may",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Wembley Stadium",
+    city: "London",
+    continent: "Europa",
     availableTickets: false,
   },
   {
     date: 20,
     month: "may",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Craven Park",
+    city: "Hull",
+    continent: "Europa",
     availableTickets: false,
   },
   {
     date: 22,
     month: "may",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Craven Park",
+    city: "Hull",
+    continent: "Europa",
     availableTickets: false,
   },
   {
     date: 5,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Gillette Stadium",
+    city: "Boston",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 7,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Gillette Stadium",
+    city: "Boston",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 8,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Gillette Stadium",
+    city: "Boston",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 15,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Sun Bowl Stadium",
+    city: "El Paso",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 17,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Sun Bowl Stadium",
+    city: "El Paso",
+    continent: "North-America",
     availableTickets: true,
   },
   {
     date: 20,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Allegiant Stadium",
+    city: "Las Vegas",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 22,
     month: "june",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Allegiant Stadium",
+    city: "Las Vegas",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 1,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Empower Field at Mile High",
+    city: "Denver",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 5,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Stanford Stadium",
+    city: "Stanford",
+    continent: "North-America",
     availableTickets: true,
   },
   {
     date: 6,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Stanford Stadium",
+    city: "Stanford",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 8,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Stanford Stadium",
+    city: "Stanford",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 29,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Rogers Stadium",
+    city: "Toronto",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 30,
     month: "july",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Rogers Stadium",
+    city: "Toronto",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 3,
     month: "august",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Nissan Stadium",
+    city: "Nashville",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 4,
     month: "august",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Nissan Stadium",
+    city: "Nashville",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 10,
     month: "august",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Hard Rock Stadium",
+    city: "Miami",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 11,
     month: "august",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Hard Rock Stadium",
+    city: "Miami",
+    continent: "North-America",
     availableTickets: false,
   },
   {
     date: 2,
     month: "september",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Narendra Modi Stadium",
+    city: "Ahmedabad",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 8,
     month: "september",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Narendra Modi Stadium",
+    city: "Ahmedabad",
+    continent: "Asia",
     availableTickets: false,
   },
   {
     date: 10,
     month: "september",
     year: 2025,
-    location: "",
-    country: "",
+    location: "Narendra Modi Stadium",
+    city: "Ahmedabad",
+    continent: "Asia",
     availableTickets: true,
   },
   {
-    date: 16,
+    date: 30,
     month: "september",
     year: 2025,
-    location: "",
-    country: "",
+    location: "City Stadium",
+    city: "Tanzania",
+    continent: "Africa",
     availableTickets: false,
   },
   {
     date: 1,
     month: "october",
     year: 2025,
-    location: "",
-    country: "",
-    availableTickets: true,
+    location: "City Stadium",
+    city: "Tanzania",
+    continent: "Africa",
+    availableTickets: false,
   },
   {
     date: 3,
     month: "october",
     year: 2025,
-    location: "",
-    country: "",
-    availableTickets: false,
+    location: "City Stadium",
+    city: "Tanzania",
+    continent: "Africa",
+    availableTickets: true,
   },
 ];
+
+// selecting elements from document
+const continentButtonsContainer = document.querySelector(".tab");
+const tourConatiner = document.querySelector(".tour");
+
+// Avalible tickets ticket buttons open window
+const openTicketSite = () => {};
+
+// displaying data from array in to elemnts and more :)
+const printTour = (toursArray) => {
+  toursArray.forEach((tour) => {
+    const singleTourContainer = document.createElement("div");
+    const dateContainer = document.createElement("div");
+    const locationContainer = document.createElement("div");
+    const continentContainer = document.createElement("div");
+    const ticketContainer = document.createElement("div");
+
+    singleTourContainer.classList.add("tour__container");
+    dateContainer.classList.add("tour__date-container");
+    locationContainer.classList.add("tour__location-container");
+    continentContainer.classList.add("tour__continent-container");
+    ticketContainer.classList.add("tour__ticket-button-container");
+
+    const date = document.createElement("p");
+    const year = document.createElement("p");
+    const location = document.createElement("p");
+    const city = document.createElement("p");
+    const ticketButton = document.createElement("button");
+
+    ticketButton.classList.add("tour__ticket-button");
+
+    date.textContent = `Date: ${tour.month} ${tour.date}`;
+    year.textContent = tour.year;
+    location.textContent = tour.location;
+    continentContainer.textContent = tour.continent;
+    city.textContent = tour.city;
+
+    if (tour.availableTickets === true) {
+      ticketButton.textContent = "Buy";
+      ticketButton.classList.add("tour__ticket-button--active");
+    } else {
+      ticketButton.textContent = "Sold out";
+      ticketButton.style.cursor = "not-allowed";
+      ticketButton.disabled;
+    }
+
+    if (ticketButton.textContent === "buy") {
+      ticketButton.addEventListener(() => {
+        openTicketSite();
+      });
+    }
+
+    dateContainer.append(date, year);
+    locationContainer.append(location, city);
+    ticketContainer.append(ticketButton);
+    singleTourContainer.append(
+      dateContainer,
+      locationContainer,
+      continentContainer,
+      ticketContainer
+    );
+    tourConatiner.append(singleTourContainer);
+  });
+};
+
+// Rednder tabs and sort by continet
+const sortTour = () => {
+  const europaButton = document.createElement("button");
+  europaButton.classList.add("tab__button");
+  europaButton.textContent = "Europa";
+
+  const asiaButton = document.createElement("button");
+  asiaButton.classList.add("tab__button");
+  asiaButton.textContent = "Asia";
+
+  const amrericaButton = document.createElement("button");
+  amrericaButton.classList.add("tab__button");
+  amrericaButton.textContent = "North-America";
+
+  const africaButton = document.createElement("button");
+  africaButton.classList.add("tab__button");
+  africaButton.textContent = "Africa";
+
+  continentButtonsContainer.append(
+    europaButton,
+    africaButton,
+    asiaButton,
+    amrericaButton
+  );
+
+  const contentButton = [];
+  contentButton.push(europaButton, africaButton, asiaButton, amrericaButton);
+
+  contentButton.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      let filterTours = [...toursArray];
+      filterTours = filterTours.filter((tour) => {
+        return tour.continent;
+      });
+
+      // singleTourContainer.classList.add("tour__container--active");
+
+      contentButton.forEach((btn) => {
+        btn.classList.remove("tab__button--active");
+      });
+      button.classList.add("tab__button--active");
+    });
+  });
+};
+
+// adds elements to page
+document.addEventListener("DOMContentLoaded", () => {
+  sortTour();
+  printTour(toursArray);
+});
